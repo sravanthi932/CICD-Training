25fcdcb8d0207bdeecb723c9384e65f0d6f8c77a
Sravanthi Anumula <sravanthi_anumula@epam.com>
2025-05-13 20:27:07 +0530

    changes about chrome binary

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -2,16 +2,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class SmokeTest {
-    WebDriver driver = null;
+    WebDriver driver;
     @BeforeTest
     public void setUpTest() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        Map<String, String> env = System.getenv();
+        String chromeBin = env.get("CHROME_BIN");
+        options.setBinary(chromeBin);
+        driver = new ChromeDriver(options);
     }
+
 
     @Test(groups = "regression")
     public void googleSearch(){
@@ -19,4 +28,13 @@ public class SmokeTest {
         System.out.println(driver.getTitle());
         driver.findElement(By.name("q")).sendKeys("lord shiva");
     }
+//    public class Main{
+//        public static void main(String args[]){
+//            ChromeOptions options = new ChromeOptions();
+//            Map<String, String> env = System.getenv();
+//            String chromeBin = env.get("CHROME_BIN");
+//            options.setBinary(chromeBin);
+//            ChromeDriver driver = new ChromeDriver(options);
+//        }
+//    }
 }
